b4620df610d0dcd1245ee81183ced6131c04917a
kevin8483 <61752808+kevin8483@users.noreply.github.com>
2022-01-09 21:29:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,6 @@ from functions.x_bar_S import x_bar_S
 # USAGE 
 mean, t_test, variance, f_test, skewness, kurtosis, KS, correlation, DTW = statistical_test(before_list, after_list, stitle="機 台 特 徵 一 相 關 性 檢 定", path="C://graph")
 group1, group2 = chamber2chamber(machines_df, stitle="機 台 的 比 較", path ="C://graph")
+ewma_value = ewma(phase1_list, phase2_list, alpha, L, stitle, xlabel, ylabel, path="C://graph/")
+t2_value = hotelling_t2(phase1_df, phase2_df, alpha, stitle, xlabel, ylabel, path="C://graph/")
 #%%
